9b8943ca6e485569aa3ddd7ced39c9abf522a39d
Nick Pestell <nick@njpestell.com>
2022-04-29 10:33:01 +0000

    initial commit

---
+++ install.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+set -e
+
+################## ARGS ##############################
+
+BIOTOOLS_PATH=~/biotools/
+
+sudo apt-get -y update && sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
+	git
+	wget
