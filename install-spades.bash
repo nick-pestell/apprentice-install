de6f3ea146865e712db38b2fc2f8a0a40bc94183
Nick Pestell <nicholas.pestell@apha.gov.uk>
2022-05-03 11:28:12 +0100

    symlink spades.py to /usr/local/bin/spades.py

--- install-spades.bash
+++ install-spades.bash
@@ -3,4 +3,4 @@
 wget https://cab.spbu.ru/files/release3.15.4/SPAdes-3.15.4-Linux.tar.gz
 tar -xf SPAdes-3.15.4-Linux.tar.gz
 rm SPAdes-3.15.4-Linux.tar.gz
-echo "export PATH=$PATH:~/biotools/SPAdes-3.15.4-Linux/bin" >> ~/.bashrc
+sudo ln -s $PWD/SPAdes-3.15.4-Linux/bin/spades.py /usr/local/bin
