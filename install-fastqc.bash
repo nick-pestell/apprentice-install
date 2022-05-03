867c9dd193239aa4224d5ba3111119c4a60ba234
Nick Pestell <nicholas.pestell@apha.gov.uk>
2022-05-03 12:12:49 +0100

    make fastqc executable

--- install-fastqc.bash
+++ install-fastqc.bash
@@ -5,4 +5,5 @@ set -e
 wget https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.9.zip
 unzip fastqc_v0.11.9.zip
 rm fastqc_v0.11.9.zip
+chmod +x $PWD/FastQC/fastqc
 sudo ln -s $PWD/FastQC/fastqc /usr/local/bin/fastqc
